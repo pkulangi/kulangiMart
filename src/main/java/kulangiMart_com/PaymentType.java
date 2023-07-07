8423613cdd2d14a88ce63f9b82ee29b786b01f1b
SamikshaPdukare <samikshadukare0@gmail.com>
2023-07-07 03:18:40 -0700

    [Samiksha] created new Branch

--- src/main/java/kulangiMart_com/PaymentType.java
+++ src/main/java/kulangiMart_com/PaymentType.java
@@ -5,7 +5,7 @@ public class PaymentType {
 	public static void main(String[] args) 
 	{
 
-		System.out.println("Everything is Free for Mockmate");
+		System.out.println("Everything is free for Mockmate");
 		
 	}
 
